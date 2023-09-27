0b41e4000a681b44f4f7d05ff61a496484c7f010
Denverite <105945495+DenveriteDotCom@users.noreply.github.com>
2023-09-27 10:32:59 -0600

    Update DenverDesignProposals.py

--- DenverDesignProposals.py
+++ DenverDesignProposals.py
@@ -68,7 +68,7 @@ browser.find_element('xpath', '//*[@id="username"]').send_keys('kevinjbeaty')
 browser.find_element('xpath', '//*[@id="passwordRequired"]').send_keys(CITYLOGIN)
 browser.find_element('xpath', '/html/body/main/app-root/div/aca-login-panel/form/div[5]/accela-button-primary/div/button/span').click()
 time.sleep(timer)
-browser.find_element('xpath','//*[@id="span_tab_1"]/table/tbody/tr/td[2]').click()
+browser.find_element('xpath','/html/body/form/div[4]/div/div[4]/div[2]/div/div/div[2]/span[1]/span[2]/table/tbody/tr').click()
 time.sleep(timer)
 
 # Select design proposals and search for all entries
