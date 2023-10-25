9963a01bea96c7243b179fd178227aa8c947f634
Denverite <105945495+DenveriteDotCom@users.noreply.github.com>
2023-10-25 15:51:25 -0600

    Update DenverDesignProposals.py

--- DenverDesignProposals.py
+++ DenverDesignProposals.py
@@ -39,8 +39,8 @@ for i in addys:
 
 timer = 5
 chrome_options = Options()
+options.add_argument("-headless")
 options = [
-    "--headless",
     "--window-size=1920,1200",
     "--ignore-certificate-errors"
 ]
